19cf451bd648e9bd2d72da58dd7c4ab411ae33cc
Sankalp Naik <naiksankalp1@gmail.com>
2025-09-06 00:28:47 -0400

    Fixed the build error.

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production'
-const repo = process.env.NEXT_PUBLIC_REPO_NAME // e.g. "sankalp-portfolio"
-
-// If deploying to https://USERNAME.github.io/REPO_NAME use basePath/assetPrefix.
-// If deploying to https://USERNAME.github.io (user/organization site), leave both undefined.
-const REPO = process.env.NEXT_PUBLIC_REPO_NAME // e.g. "my-portfolio" (optional)
+const repo = process.env.NEXT_PUBLIC_REPO_NAME
 
 module.exports = {
-  output: 'export',                // enables `next export`
-  images: { unoptimized: true },   // Next/Image works on static hosts
+  output: 'export',        
+  images: { unoptimized: true },
   basePath: isProd && repo ? `/${repo}` : undefined,
   assetPrefix: isProd && repo ? `/${repo}/` : undefined,
 }
