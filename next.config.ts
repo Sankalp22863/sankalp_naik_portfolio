30bdac06c947e2daa735acd329c814f88cff16c7
Sankalp Naik <naiksankalp1@gmail.com>
2025-09-06 00:19:21 -0400

    Fixed the image issue.

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production'
+const repo = process.env.NEXT_PUBLIC_REPO_NAME // e.g. "sankalp-portfolio"
 
 // If deploying to https://USERNAME.github.io/REPO_NAME use basePath/assetPrefix.
 // If deploying to https://USERNAME.github.io (user/organization site), leave both undefined.
@@ -8,7 +9,6 @@ const REPO = process.env.NEXT_PUBLIC_REPO_NAME // e.g. "my-portfolio" (optional)
 module.exports = {
   output: 'export',                // enables `next export`
   images: { unoptimized: true },   // Next/Image works on static hosts
-  // Uncomment if deploying to a project page at /REPO:
-  // basePath: isProd && REPO ? `/${REPO}` : undefined,
-  // assetPrefix: isProd && REPO ? `/${REPO}/` : undefined,
+  basePath: isProd && repo ? `/${repo}` : undefined,
+  assetPrefix: isProd && repo ? `/${repo}/` : undefined,
 }
