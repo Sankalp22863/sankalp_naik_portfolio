b65db2be14b58b9db28b55aeee1f9d8f21eaefbe
Sankalp Naik <naiksankalp1@gmail.com>
2025-09-07 14:49:54 -0400

    Fixed the build

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production'
-const repo = process.env.NEXT_PUBLIC_REPO_NAME
 const base = ""
 
-module.exports = {
+const config = {
   output: 'export',        
   images: { unoptimized: true },
   basePath: isProd && base ? base : undefined,
-  assetPrefix: isProd && repo ? `/${repo}/` : undefined,
+  assetPrefix: isProd && base ? `/${base}/` : undefined,
 }
+
+
+module.exports = config
