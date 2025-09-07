7bf5f1059d878796f00b83c585731d086e0d7929
Sankalp Naik <naiksankalp1@gmail.com>
2025-09-07 14:38:54 -0400

    Added custom domain

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production'
 const repo = process.env.NEXT_PUBLIC_REPO_NAME
+const base = ""
 
 module.exports = {
   output: 'export',        
   images: { unoptimized: true },
-  basePath: '/',
+  basePath: isProd && base ? base : undefined,
   assetPrefix: isProd && repo ? `/${repo}/` : undefined,
 }
